0f9558d475e5e72a41894d89b98b728bad6ee91b
Nathan Fuller <fatenuller@gmail.com>
2014-02-02 14:21:15 -0500

    initial commit

--- FirstProject/ViewController.m
+++ FirstProject/ViewController.m
@@ -10,13 +10,30 @@
 
 @interface ViewController ()
 
+@property (assign) bool clickable;
+@property (assign) int clickCount;
+@property (strong, nonatomic) UIImageView *labelBackground;
+@property (weak, nonatomic) IBOutlet UILabel *theLabel;
+- (IBAction)theButton:(id)sender;
+
 @end
 
 @implementation ViewController
 
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.clickable = true;
+    self.clickCount = 0;
+    [self.theLabel setAlpha:(0.0f)];
+    self.labelBackground = [[UIImageView alloc]
+                                    initWithImage:[UIImage imageNamed:@"kitty00.jpg"]];
+    self.labelBackground.frame = self.theLabel.frame;
+    [self.theLabel addSubview: self.labelBackground];
+    self.theLabel.backgroundColor = [UIColor clearColor];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -26,4 +43,48 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)fadeInLabels
+{
+    self.clickable = false;
+    [UIView animateWithDuration:1.0
+                          delay:0.0
+                        options:UIViewAnimationOptionCurveEaseIn
+                     animations:^ {[self.theLabel setAlpha:(1.0f)];}
+                     completion:^(BOOL finished) {
+                         [self.theLabel setAlpha:(1.0f)];
+                         [self fadeOutLabels];
+                     }];
+}
+
+- (void)fadeOutLabels
+{
+    [UIView animateWithDuration:1.0
+                          delay:1.0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^ {[self.theLabel setAlpha:(0.0f)];}
+                     completion:^(BOOL finished) {
+                         [self.theLabel setAlpha:(0.0f)];
+                         self.clickable = true;
+                     }];
+}
+
+- (void)changeKitty
+{
+    [self.labelBackground setImage:[UIImage imageNamed:
+                            [NSString stringWithFormat:@"kitty0%d.jpg", (self.clickCount) % 8]]];
+    [self.theLabel addSubview: self.labelBackground];
+    self.clickCount += 1;
+}
+
+- (IBAction)theButton:(id)sender
+{
+    if(self.clickable == true){
+        self.clickable = false;
+        [self.theLabel setAlpha:(0.0f)];
+        // [self.theLabel setText:(@"hello")];
+        [NSTimer scheduledTimerWithTimeInterval:0.0 target:self selector:@selector(fadeInLabels) userInfo:nil repeats:NO];
+        //[NSTimer scheduledTimerWithTimeInterval:2.0 target:self selector:@selector(fadeOutLabels) userInfo:nil repeats:NO];
+        [self changeKitty];
+    }
+}
 @end
